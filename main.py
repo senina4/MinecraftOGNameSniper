9712331977558643433a0c221ed0c0533f6df9bd
Sean TW <84493324+senina4@users.noreply.github.com>
2024-01-12 13:56:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ try:
     from colorama import Fore, Back, just_fix_windows_console
     just_fix_windows_console()
     from datetime import datetime
+
+    
     
     def msg(tittle, submsg):
         return f"{Fore.CYAN}{Back.RESET}{datetime.now().hour}-{datetime.now().minute}-{datetime.now().second} {Fore.BLUE}{tittle}{Fore.RESET}: {Fore.GREEN}{submsg}{Fore.RESET}"
-        
     bearer_token = input(msg("登入", "請輸入 bearer token(你可以在'https://senina4.github.io/OGName/'找到如何獲取bearer token): "))
     client = cl(bearer_token=bearer_token)
-    newname = input(msg("系統", "輸入你想要的新名稱: ")
+    newname = input(msg("系統", "輸入你想要的新名稱: "))
     api = mcapi()
     uuid = api.get_uuid(client.get_profile().name)
     times = 0
